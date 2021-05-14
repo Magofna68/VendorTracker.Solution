102ce7659edf1cd6eec5fb0f7323184972aa9377
Brandon-Magofna <magofna68@gmail.com>
2021-05-14 10:20:23 -0700

    :white_check_mark: 5th passing test; getall method & clearall method included

--- VendorTracker.Solution/VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Solution/VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -6,12 +6,12 @@ using System.Collections.Generic;
 namespace VendorTracker.Tests //Change to match Title
 {
   [TestClass]
-  public class OrderTests //: IDisposable
+  public class OrderTests : IDisposable
   {
-    // public void Dispose()
-    // {
-    //   record.ClearAll();
-    // }
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
 
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
@@ -51,6 +51,23 @@ namespace VendorTracker.Tests //Change to match Title
     {
       List<Vendor> newList = new List<Vendor> { };
       List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllVendors_Vendors()
+    {
+      string vName = "Pierre";
+      string vDes = "baker";
+      string vName2 = "Margret";
+      string vDes2 = "chef";
+
+      Vendor newVendor = new Vendor(vName, vDes);
+      Vendor newVendor2 = new Vendor(vName2, vDes2);
+      List<Vendor> newList = new List<Vendor> { newVendor, newVendor2 };
+
+      List<Vendor> result = Vendor.GetAll();
+
       CollectionAssert.AreEqual(newList, result);
     }
   }
