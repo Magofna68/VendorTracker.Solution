6bb9d8b62b0faae08246470f52aa3d08763804d7
Brandon-Magofna <magofna68@gmail.com>
2021-05-14 12:32:40 -0700

    :white_check_mark: 9th passing test: add properties to constructor

--- VendorTracker.Solution/VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Solution/VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -17,8 +17,20 @@ namespace VendorTrackerTests //Change to match Title
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("Pierre");
+      Order newOrder = new Order("Pierre", 0, 0, "");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfAllProperties_Order()
+    {
+      string title = "Chocolate Chip Cookies";
+      int quantity = 24;
+      int price = 10;
+      string date = "June 10th";
+      Order newOrder = new Order(title, quantity, price, date);
+
+      Assert.AreEqual(date, newOrder.Date);
     }
   }
 }
--- VendorTracker.Solution/VendorTracker/Models/Order.cs
+++ VendorTracker.Solution/VendorTracker/Models/Order.cs
@@ -5,19 +5,19 @@ namespace VendorTracker.Models
   public class Order
   {
     public string Title { get; set; }
-    public string Description { get; set; }
+    public int Quantity { get; set; }
     public int Price { get; set; }
     public string Date { get; set; }
     // public int Id { get; }
 
     private static List<Order> _instances = new List<Order> { };
 
-    public Order(string oTitle) //string oDescription, int oPrice, string oDate)
+    public Order(string oTitle, int oQuantity, int oPrice, string oDate)
     {
       Title = oTitle;
-      // Description = oDescription;
-      // Price = oPrice;
-      // Date = oDate;
+      Quantity = oQuantity;
+      Price = oPrice;
+      Date = oDate;
       // _instances.Add(this);
       // Id = _instances.Count;
     }
